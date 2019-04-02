26c9b3afeab3063ac63fcf00b99eda570d00e325
kozooon <kozooonkt@gmail.com>
2019-04-02 22:08:42 +0900

    composer.pharを配置

---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>商品管理アプリ</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>商品管理アプリ</h1>
+</header>
+<div class="container">
+    ここにリンク入れる
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>商品名</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>パソコン</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
